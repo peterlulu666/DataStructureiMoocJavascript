d7139aaa0103d4ebc9261ff05df1a42075b750ea
vpslinuxinstall <shoparoundwyz@gmail.com>
2019-12-26 13:26:40 -0500

    commit

--- LinkedList/SinglyLinkedList/SinglyLinkedList.js
+++ LinkedList/SinglyLinkedList/SinglyLinkedList.js
@@ -1,5 +1,6 @@
 class Node {
 
+    // Node contains data and next pointer
     constructor(data) {
         this.data = data;
         this.next = null;
@@ -11,33 +12,52 @@ class Node {
 class SinglyLinkedList {
 
     constructor() {
+        // Let the head pointer pointing to null
         this.head = null;
+        // Let the tail pointer pointing to null
         this.tail = null;
+        // Let the size to be 0
         this.currentSize = 0;
 
     }
 
+    // Check if the linked list is empty
     isEmpty() {
         return (this.head === null && this.tail === null);
 
     }
 
+    // The current size of linked list
     getSize() {
         return this.currentSize;
 
     }
 
+    // Add node to the first node
     addLast(data) {
+        // Create a new node
         var newNode = new Node(data);
+        // If the linked list is empty
         if (this.isEmpty()) {
+            // Copy the address of newNode to head pointer
+            // Let the head pointer pointing to new node
             this.head = newNode;
+            // Copy the address of newNode to tail pointer
+            // Let the tail pointer pointing to new node
             this.tail = newNode;
+            // Increment the size
             this.currentSize++;
             return;
 
         }
+        // If the linked list is not empty
+        // Copy the address of newNode to last node next pointer
+        // Let the last node pointer pointing to newNode
         this.tail.next = newNode;
+        // Copy the address of newNode tail pointer
+        // Let the tail pointer pointing to newNode
         this.tail = newNode;
+        // Increment the size
         this.currentSize++;
 
     }
