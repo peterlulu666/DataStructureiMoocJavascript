cb343cc5a9cfb7fd76347f913b1ac90c08f9bac5
vpslinuxinstall <shoparoundwyz@gmail.com>
2020-02-24 11:56:27 -0500

    commit

--- StackQueue/StackSinglyLinkedListImplement.js
+++ StackQueue/StackSinglyLinkedListImplement.js
@@ -48,32 +48,50 @@ class StackSinglyLinkedListImplement {
 
         }
 
-        // If the stack has one node
-        if (this.currentSize === 1) {
-            // Copy the head and store the address to tmpPointer
-            var tmpPointer = this.head;
-            // Let head point to null
-            this.head = null;
-            // Let tail point to null
-            this.tail = null;
-            // Decrement the currentSize
-            this.currentSize--;
-            // Return removed data
-            return tmpPointer.data;
-
-        }
-
-        // If the stack has more than one node
+        // If the stack is not empty
         // Copy the head pointer and store the address to tmpPointer
         var tmpPointer = this.head;
+        // If the stack has one node
+        if (this.currentSize === 1) {
+            // Let the tail pointer point to null
+            this.tail = null;
+
+        }
         // Move the head pointer
         this.head = this.head.next;
         // Let tmpPointer.next point to null
         tmpPointer.next = null;
         // Decrement the currentSize
         this.currentSize--;
-        // Return removed data
+        // Return the removed data
         return tmpPointer.data;
+
+        // // If the stack has one node
+        // if (this.currentSize === 1) {
+        //     // Copy the head and store the address to tmpPointer
+        //     var tmpPointer = this.head;
+        //     // Let head point to null
+        //     this.head = null;
+        //     // Let tail point to null
+        //     this.tail = null;
+        //     // Decrement the currentSize
+        //     this.currentSize--;
+        //     // Return removed data
+        //     return tmpPointer.data;
+        //
+        // }
+        //
+        // // If the stack has more than one node
+        // // Copy the head pointer and store the address to tmpPointer
+        // var tmpPointer = this.head;
+        // // Move the head pointer
+        // this.head = this.head.next;
+        // // Let tmpPointer.next point to null
+        // tmpPointer.next = null;
+        // // Decrement the currentSize
+        // this.currentSize--;
+        // // Return removed data
+        // return tmpPointer.data;
 
     }
 
@@ -98,6 +116,8 @@ singlyLinkedListImplement.push(3);
 singlyLinkedListImplement.show();
 console.log(singlyLinkedListImplement.pop());
 singlyLinkedListImplement.show();
+
+
 
 
 
