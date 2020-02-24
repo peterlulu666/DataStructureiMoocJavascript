1ba08784b89d9292adcb0815dde7372187cea26e
vpslinuxinstall <shoparoundwyz@gmail.com>
2020-02-24 12:13:11 -0500

    commit

---
+++ StackQueue/Queue.js
@@ -0,0 +1,26 @@
+var queue = [];
+queue.push("1 <- ");
+queue.push("2 <- ");
+queue.push("3");
+console.log(queue);
+console.log(queue.shift());
+console.log(queue);
+
+var queueArray = [];
+queueArray.unshift("-> 1");
+queueArray.unshift(" -> 2");
+queueArray.unshift("3");
+console.log(queueArray);
+console.log(queueArray.pop());
+console.log(queueArray);
+
+
+
+
+
+
+
+
+
+
+
